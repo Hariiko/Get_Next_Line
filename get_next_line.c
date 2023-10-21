fe1450e2a330e77086acbc01df8bdd426899f41c
Laura Altarriba Villellas <laltarri@car1s1.42barcelona.com>
2023-10-21 16:30:25 +0200

    modificate

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,41 @@
 /*   By: laltarri <laltarri@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 16:44:17 by laltarri          #+#    #+#             */
-/*   Updated: 2023/10/18 18:26:33 by laltarri         ###   ########.fr       */
+/*   Updated: 2023/10/20 20:30:54 by laltarri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+	
+//Esta funcion tiene que cre:qar un  nuevo storage, que no contenga la linea extrada,
+//y solo guarde e; troxo de informaciuon que queremos guardar
+// storage -> "hola que tal\n bien\0"
+// new_storage -> " bien\0"
+/*char *update_storage(char *storage)
+{
+		char *new_storge
 
-char *read_file(int fd, char *buffer)
+		//calcularia el tamano necesario
+		//copiaria lo que quiero guardar
+
+		free(storage);
+		return(new_storage);
+}*/
+
+// Esto tiene que llenar el storage con sufieciente informacion como para que luego 
+// podamos extraer una linea. LLnearemos storage hasta que haya un \n, o num_bytes = 0,
+// que significaria que hemos llegado al final del archivo
+char *read_file(int fd, char *storage)
 {
 	int i;
-
+	char *buffer;
 	buffer = malloc(BUFFER_SIZE + 1 * sizeof(char));
 	if (!buffer)
 		return (NULL);
 	buffer[BUFFER_SIZE] = '\0';
 	i = 1;
-	while(i != 0)
+
+	while(i > 0 && !ft_strchr(buffer, '\n'))
 	{
 		i = read(fd, buffer, BUFFER_SIZE);	
 		if (i == -1)
@@ -29,24 +48,30 @@ char *read_file(int fd, char *buffer)
 			free(buffer);
 			return (NULL);
 		}
-//		write(1, "a", 1);
+		storage = ft_strjoin(storage, buffer);
 	}
-	return (buffer);
+	return (storage);
 }
 
 char *get_next_line(int fd)
 {
-	static char *buffer;	
-	
+	static char *storage = NULL;	
+	char 		*line;
+
 	if (!fd || BUFFER_SIZE == 0)
 		return (NULL);
-	if(!buffer || (buffer && !ft_strchr(buffer, '\n')))
-		buffer = read_file(fd, buffer);
-	if (!buffer)
+	if(!storage || (storage && !ft_strchr(storage, '\n')))
+		storage = read_file(fd, storage);
+	if (!storage)
 		return (NULL);
-	return (buffer);
+	// Necesito extraer una linea de storage
+	// line = extract_line(storage);
+	// if(!line)
+	//  return(NULL)
+	// storage = update_storage(storage)
+	return (line);
 }
-/*
+
 int main ()
 {
 	int file;
@@ -55,8 +80,9 @@ int main ()
 	if (!file || file == -1)
 		printf("ERROR");
 	readfile = get_next_line(file);
+	printf("%s\n", readfile);
 	printf("%d\n", file);
 	printf("%d\n", BUFFER_SIZE);
 	close(file);
 	return 0;
-}*/
+}
